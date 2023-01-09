a8ad752fa432c654698da622debf97336767e72a
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-01-09 22:20:45 +0200

    sss done

--- secret sharing/SSS.py
+++ secret sharing/SSS.py
@@ -1,7 +1,7 @@
 from sympy import GF, Poly
 import random
 from numpy.polynomial import Polynomial
-# from scipy.interpolate import lagrange
+from scipy.interpolate import lagrange
 
 
 class SSS:
@@ -10,20 +10,6 @@ class SSS:
         self.t = t # threshold where t <= n 
         self.prime = prime # prime number
         self.field = GF(prime) # finite field
-
-    # def generate_shares(self):
-    #     # generate random coefficients
-    #     coefficients = [random.randint(1,self.prime-1) for i in range(self.t-1)]
-    #     coefficients.append(self.secret)
-    #     print(coefficients)
-
-    #     # generate polynomial
-    #     polynomial = Poly(coefficients, self.field)
-    #     # generate shares
-    #     shares = []
-    #     for i in range(1,self.n+1):
-    #         shares.append((i,polynomial(i)))
-    #     return shares
 
     def generate_shares(self, secret):
         # generate random coefficients
@@ -40,15 +26,15 @@ class SSS:
 
     def recover_secret(self, shares):
         # recover secret
-        # polynomial = lagrange([i[0] for i in shares], [i[1] for i in shares])
-        # return polynomial(0)
-        polynomial = Polynomial.fit([i[0] for i in shares], [i[1] for i in shares], self.t-1)
-        return polynomial(0)
+        polynomial = lagrange([i[0] for i in shares], [i[1] for i in shares])
+        print(polynomial)
+        return int(polynomial.coeffs[0])
+
 
 if __name__ == "__main__":
-    sss = SSS(5,3,7)
-    shares = sss.generate_shares(14)
-    print(shares)
-    secret = sss.recover_secret(shares)
-    print(secret)
+    sss = SSS(20,18,7)
+    shares = sss.generate_shares(141592)
+    # print(shares)
+    secret = sss.recover_secret(shares[:18])
+    print((secret))
 
